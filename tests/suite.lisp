f2b63184207ff9d479c7f2d1bd5bb0e3db0ec1e0
Adlai <munchking@gmail.com>
2009-07-19 04:28:12 +0300

    Fixed unit tests

--- tests/suite.lisp
+++ tests/suite.lisp
@@ -4,18 +4,18 @@
 
 (def-suite sykobot-tests)
 (defun run-all-tests ()
-  (run! 'sykobot-tests))
+  (5am:run! 'sykobot-tests))
 (defmethod asdf:perform ((o asdf:test-op) (c (eql (asdf:find-system :sykobot-tests))))
-  (format t "~&~%*******************~%~
-                 ** Starting test **~%~
+  (format t "~&~%*******************~@
+                 ** Starting test **~@
                  *******************~%")
   (run-all-tests)
-  (format t "~&*****************************************~%~
-               **            Tests finished           **~%~
-               *****************************************~%~
-               ** If there were any failures on your  **~%~
-               ** platform, please report them to me: **~%~
-               **  (sykopomp at sykosomatic dot org)  **~%~
-               ** or just file a bugreport on github: **~%~
-               ** github.com/sykopomp/sykobot/issues  **~%~
+  (format t "~&*****************************************~@
+               **            Tests finished           **~@
+               *****************************************~@
+               ** If there were any failures on your  **~@
+               ** platform, please report them to me: **~@
+               **  (sykopomp at sykosomatic dot org)  **~@
+               ** or just file a bugreport on github: **~@
+               ** github.com/sykopomp/sykobot/issues  **~@
                *****************************************~%"))
